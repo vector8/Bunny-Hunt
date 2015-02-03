62834a56ca1a1147ad2570c339f5f2274c863674
nikeng <nikeng@localhost>
2015-02-02 23:20:21 -0500

    PEW PEW all da changes is hurr hunger, day msgs, carrots (ish), increased size of all things, for some reason player won't increase tho :(

--- UnityProject/Assets/Scripts/PlayerTrigger.cs
+++ UnityProject/Assets/Scripts/PlayerTrigger.cs
@@ -1,33 +1,49 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class PlayerTrigger : MonoBehaviour
 {
 	public Sundial sundial;
 	public GameController gameController;
+	public Hunger hungerController;
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		Debug.Log(other.tag);
+		//Debug.Log(other.tag);
 		if(other.tag == "Enemy")
 		{
 			if(sundial.isDayTime())
 			{
 				// kill the player
 				gameObject.SetActive(false);
-			}
-			else
+			} else
 			{
 				// eat the enemy
 				Destroy(other.gameObject);
-				// gain point
+				// gain point and update hunger
 				gameController.hunters++;
+				if(hungerController.hunger < 70)
+				{
+					hungerController.hunger += 30;
+				} else
+				{
+					hungerController.hunger = 100;
+				}
 			}
-		} else
+		} else if(other.tag == "Carrot")
 		{
 			// eat the carrot
 			Destroy(other.gameObject);
+			// gain point and update hunger
 			gameController.carrots++;
+			if(hungerController.hunger < 70)
+			{
+				hungerController.hunger += 30;
+			} else
+			{
+				hungerController.hunger = 100;
+			}
+
 		}
 	}
 	
