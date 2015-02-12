e8bc1e0bfa7aca35c378076c2561c3f85de695db
yyfang <yyfang@localhost>
2015-02-11 23:22:58 -0500

    added Title screen, pause screen fixed game over menu button and txt sizes

---
+++ UnityProject/Assets/Scripts/Pause.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+	private bool pauseGame;
+	private bool showGUI;
+	public GameObject pause;
+	public GameObject pauseGUI;
+
+	public void ResumeGame(){
+		pauseGUI.SetActive(false);
+		pause.SetActive(true);
+		Time.timeScale=1;
+		pauseGame = false;
+
+	}
+
+	public void PauseGame()
+	{
+		pauseGame = true;
+		Time.timeScale=0;
+		pause.SetActive(false);
+		pauseGUI.SetActive(true);
+	}
+
+	public bool isGamePasued(){
+		return pauseGame;
+	}
+
+	public void ExitGame(){
+		Application.LoadLevel("TitleScreen");
+	}
+}
---
+++ unity backup/UnityProject/Assets/Scripts/Spear.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spear : MonoBehaviour
+{
+	private float u = 0.0f;
+	private Vector3 originalPosition;
+	private bool finished = false;
+	private float interpSpeed;
+	private float lifeTime;
+	private Vector3 target;
+	public float maxLifeTime;
+	public float moveSpeed;
+	public GameObject player;
+	public GameController gameController;
+	
+	// Use this for initialization
+	void Start()
+	{
+		originalPosition = transform.position;
+		target = player.transform.position;
+		
+		interpSpeed = moveSpeed / Vector3.Distance(originalPosition, target);
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		lifeTime += Time.deltaTime;
+		u += interpSpeed * Time.deltaTime;
+		
+		if(u >= 1.0f)
+		{
+			u = 1.0f;
+			finished = true;
+		} else if(lifeTime >= maxLifeTime)
+		{
+			finished = true;
+		}
+		
+		transform.position = Vector3.Lerp(originalPosition, target, u);
+		
+		if(Vector3.Distance(player.transform.position, transform.position) < 0.1)
+		{
+			if(!gameController.IsReviving()){
+				player.SetActive(false);
+				gameController.GameOverDisplay();
+			}else{
+				Debug.Log("resetting revival");
+				StartCoroutine(gameController.Wait());
+				gameController.Reviving(false);
+			}
+			// remove this spear
+			Destroy(gameObject);
+		} else if(finished)
+		{
+			Destroy(gameObject);
+		}
+		
+	}
+}
