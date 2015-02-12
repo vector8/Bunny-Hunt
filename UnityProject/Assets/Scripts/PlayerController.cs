10031c5e6cb52f68db157f7ef8a72ee1a05ef2d8
yyfang <yyfang@localhost>
2015-02-11 23:17:36 -0500

    added title screen, pause screen fixed restart menu buttons size

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour
 	private Animator anim;
 	private Vector3 moveDirection;
 	private Vector3 targetPosition;
+	private bool canMove = true;
 
 	public float hunger = 100;
 	public float hungerFactor;
@@ -31,137 +32,151 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		Vector3 currentPosition = transform.position;
+		if(canMove == true)
+		{
+			Vector3 currentPosition = transform.position;
 		
-		if(Input.GetButtonDown("Fire1"))
-		{
-			clickDelay = 0;
-		} else if(Input.GetButtonUp("Fire1"))
-		{
-			jumping = (clickDelay < 0.5f);
-		}
+			if(Input.GetButtonDown("Fire1"))
+			{
+				clickDelay = 0;
+			} else if(Input.GetButtonUp("Fire1"))
+			{
+				jumping = (clickDelay < 0.5f);
+			}
 
-		if(jumping)
-		{
-			jumpTimer += Time.deltaTime;
-			
-			anim.SetFloat(speedHash, jumpSpeed);
-			transform.position += moveDirection * jumpSpeed * Time.deltaTime;
-			
-			float screenWidth, screenHeight;
-			screenHeight = Camera.main.orthographicSize;
-			screenWidth = Camera.main.aspect * screenHeight;
-			
-			Vector3 pos = transform.position;
-			
-			if(pos.x > screenWidth)
+			if(jumping && canMove)
 			{
-				pos.x = screenWidth;
-			} else if(pos.x < -screenWidth)
-			{
-				pos.x = -screenWidth;
-			}
-			if(pos.y > screenHeight)
-			{
-				pos.y = screenHeight;
-			} else if(pos.y < -screenHeight)
-			{
-				pos.y = -screenHeight;
-			}
+				jumpTimer += Time.deltaTime;
 			
-			transform.position = pos;
+				anim.SetFloat(speedHash, jumpSpeed);
+				transform.position += moveDirection * jumpSpeed * Time.deltaTime;
 			
-			if(jumpTimer >= jumpDuration)
-			{
-				moveDirection.x = 0;
-				moveDirection.y = 0;
-				moveDirection.z = 0;
-				
-				jumping = false;
-				jumpTimer = 0;
-			}
-		} else
-		{
-			if(Input.GetButton("Fire1"))
-			{
-				clickDelay += Time.deltaTime;
-				
-				targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-				
 				float screenWidth, screenHeight;
 				screenHeight = Camera.main.orthographicSize;
 				screenWidth = Camera.main.aspect * screenHeight;
-				
-				if(targetPosition.x > screenWidth)
+			
+				Vector3 pos = transform.position;
+			
+				if(pos.x > screenWidth)
 				{
-					targetPosition.x = screenWidth;
-				} else if(targetPosition.x < -screenWidth)
+					pos.x = screenWidth;
+				} else if(pos.x < -screenWidth)
 				{
-					targetPosition.x = -screenWidth;
+					pos.x = -screenWidth;
 				}
-				
-				if(targetPosition.y > screenHeight)
+				if(pos.y > screenHeight)
 				{
-					targetPosition.y = screenHeight;
-				} else if(targetPosition.y < -screenHeight)
+					pos.y = screenHeight;
+				} else if(pos.y < -screenHeight)
 				{
-					targetPosition.y = -screenHeight;
+					pos.y = -screenHeight;
 				}
-				
-				targetPosition.z = 0;
-				
-				if(Vector3.Distance(targetPosition, transform.position) >= 0.1)
+			
+				transform.position = pos;
+			
+				if(jumpTimer >= jumpDuration)
 				{
-					moveDirection = targetPosition - currentPosition;
+					moveDirection.x = 0;
+					moveDirection.y = 0;
 					moveDirection.z = 0;
-					moveDirection.Normalize();
+				
+					jumping = false;
+					jumpTimer = 0;
 				}
 			} else
 			{
-				moveDirection.x = 0;
-				moveDirection.y = 0;
-				moveDirection.z = 0;
-			}
-		
-			if(moveDirection.magnitude != 0)
-			{
-				Vector3 scale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-				if(moveDirection.x < 0)
+				if(Input.GetButton("Fire1"))
 				{
-					scale.x *= -1;
-				}
-				transform.localScale = scale;
+					clickDelay += Time.deltaTime;
 				
-				anim.SetFloat(speedHash, moveSpeed);
-				transform.position += moveDirection * moveSpeed * Time.deltaTime;
+					targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 				
-				if(Vector3.Distance(targetPosition, transform.position) < 0.1)
+					float screenWidth, screenHeight;
+					screenHeight = Camera.main.orthographicSize;
+					screenWidth = Camera.main.aspect * screenHeight;
+				
+					if(targetPosition.x > screenWidth)
+					{
+						targetPosition.x = screenWidth;
+					} else if(targetPosition.x < -screenWidth)
+					{
+						targetPosition.x = -screenWidth;
+					}
+				
+					if(targetPosition.y > screenHeight)
+					{
+						targetPosition.y = screenHeight;
+					} else if(targetPosition.y < -screenHeight)
+					{
+						targetPosition.y = -screenHeight;
+					}
+				
+					targetPosition.z = 0;
+				
+					if(Vector3.Distance(targetPosition, transform.position) >= 0.1)
+					{
+						moveDirection = targetPosition - currentPosition;
+						moveDirection.z = 0;
+						moveDirection.Normalize();
+					}
+				} else
 				{
 					moveDirection.x = 0;
 					moveDirection.y = 0;
 					moveDirection.z = 0;
 				}
+		
+				if((moveDirection.magnitude != 0)&& canMove)
+				{
+					Vector3 scale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+					if(moveDirection.x < 0)
+					{
+						scale.x *= -1;
+					}
+					transform.localScale = scale;
+				
+					anim.SetFloat(speedHash, moveSpeed);
+					transform.position += moveDirection * moveSpeed * Time.deltaTime;
+				
+					if(Vector3.Distance(targetPosition, transform.position) < 0.1)
+					{
+						moveDirection.x = 0;
+						moveDirection.y = 0;
+						moveDirection.z = 0;
+					}
+				} else
+				{
+					anim.SetFloat(speedHash, 0);
+				}
+			}
+		
+			if(!sundial.isDayTime())
+			{
+				transform.localScale = new Vector3(Mathf.Sign(transform.localScale.x) * 3.0f, 3.0f, 1.0f);
 			} else
 			{
-				anim.SetFloat(speedHash, 0);
+				transform.localScale = new Vector3(Mathf.Sign(transform.localScale.x) * 1.5f, 1.5f, 1.0f);
+			}
+		
+			anim.SetBool(mutatedHash, !sundial.isDayTime());
+
+			if(this.gameObject.activeSelf)
+			{
+				UpdateHunger();
 			}
 		}
-		
-		if(!sundial.isDayTime())
-		{
-			transform.localScale = new Vector3(Mathf.Sign(transform.localScale.x) * 3.0f, 3.0f, 1.0f);
-		}
-		else
-		{
-			transform.localScale = new Vector3(Mathf.Sign(transform.localScale.x) * 1.5f, 1.5f, 1.0f);
-		}
-		
-		anim.SetBool(mutatedHash, !sundial.isDayTime());
+	}
+	void Move(){
 
-		if(this.gameObject.activeSelf)
-		{
-			UpdateHunger();
-		}
+	}
+
+	public void stopMove(){
+		print("stop moving");
+		canMove = false;
+	}
+	public void resumeMove(){
+		print("resume moving");
+		canMove = true;
 	}
 
 	void UpdateHunger()
