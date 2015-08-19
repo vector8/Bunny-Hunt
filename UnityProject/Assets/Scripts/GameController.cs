00ca03b92e604215466800c6b5da00c00bd340b7
vector8 <vector8@localhost>
2015-08-18 20:58:42 -0400

    Changed achievements and leaderboard button sprites. Merged "sprites" and "sprites - new" folders and cleaned up unused files. Added achievement button to gameover screen and switched places of leaderboard and exit buttons. Also cleaned up spacing on gameover screen. Cleaned up gameover screen to toggle only parent active/inactive. Modified main scene to make sundial a canvas based image like everything else. (Removed funky in-script resizing accordingly.) Also made the sky darker at night. 'Cause I can do that.

--- UnityProject/Assets/Scripts/GameController.cs
+++ UnityProject/Assets/Scripts/GameController.cs
@@ -3,199 +3,195 @@ using UnityEngine.UI;
 using UnityEngine.Advertisements;
 using System.Collections;
 
-public class GameController : MonoBehaviour {
-	
-	public float timeOfDay;
-	public GameObject player;
-	public string appID;
-	public int carrots = 0;
-	public int hunters = 0;
-	public bool lifeAvailable = true;
+public class GameController : MonoBehaviour
+{
 
-	public Text txtDayCount;
-	public Text txtGameOver; 
-	public Text txtCarrots;
-	public Text txtDays;
-	public Text txtHunters;
-	public Text txtDayMsg;
-	public Text carrotsCount;
-	public Text huntersCount;
-	public Button btnRestart;
-	public Button btnShowAd;
-	public Button btnShowLB;
-	public Sundial sundial;
-	public Camera camera;
-	public GameObject sundialUI;
-	public GameObject confirmLogin;
-	public AudioSource gameMusic;
-	public Button btnExit;
-	public Button btnPause;
+    public float timeOfDay;
+    public GameObject player;
+    public string appID;
+    public int carrots = 0;
+    public int hunters = 0;
+    public bool lifeAvailable = true;
 
-	private GameObject googlePlayObj;
-	private Googleplay googlePlayScript;
-	private bool dayTime = true;
-	private bool fade = true;
-	private bool displayMsg = false;
-	private bool reviving = false;
-	private int preCarrots = 0;
-	private int preHunters = 0;
-	private int preDay = 0;
-	
-	// Use this for initialization
-	void Start()
-	{
-		if (Advertisement.isSupported) {
-			Advertisement.allowPrecache = true;
-			Advertisement.Initialize (appID, false);
-		}			
-		googlePlayObj = GameObject.Find("GooglePlay");
-		googlePlayScript = googlePlayObj.GetComponent<Googleplay>();
-	}
-	
-	// Update is called once per physics time unit
-	void FixedUpdate()
-	{
-		//update day count
-		txtDayCount.text = "Day " + sundial.day.ToString();
-		//find sundial location and place Day # relative to it
-		Vector3 screenPos = camera.WorldToScreenPoint(sundialUI.transform.position);
-		Vector3 origin = Camera.main.WorldToScreenPoint(new Vector3(0f, sundialUI.transform.renderer.bounds.max.y, 0f));
-		Vector3 extent = Camera.main.WorldToScreenPoint(new Vector3(0f, sundialUI.transform.renderer.bounds.min.y, 0f));
-		screenPos.y = screenPos.y + (extent.y - origin.y)/5;
-		txtDayCount.transform.position = screenPos;
+    public Text txtDayCount;
+    public Text txtGameOver;
+    public Text txtCarrots;
+    public Text txtDays;
+    public Text txtHunters;
+    public Text txtDayMsg;
+    public Text carrotsCount;
+    public Text huntersCount;
+    public Button btnShowAd;
+    public Sundial sundial;
+    public Camera camera;
+    public GameObject sundialUI;
+    public GameObject confirmLogin;
+    public GameObject gameOverDisplay;
+    public AudioSource gameMusic;
+    public Button btnPause;
 
-		if(player.activeSelf)
-		{
-			if (dayTime != sundial.isDayTime())	// if daytime has changed, update display msg
-			{
-				dayTime = sundial.isDayTime();
-				fade = true;
-				displayMsg = true;
+    private GameObject googlePlayObj;
+    private Googleplay googlePlayScript;
+    private bool dayTime = true;
+    private bool fade = true;
+    private bool displayMsg = false;
+    private bool reviving = false;
+    private int preCarrots = 0;
+    private int preHunters = 0;
+    private int preDay = 0;
 
-				if (dayTime)
-				{
-					txtDayMsg.text = "Day " + sundial.day + "\n"
-									+ "Run!";
-				} else
-				{
-					txtDayMsg.text = "Night " + sundial.day + "\n"
-									+ "Hunt!";
-				}
-			}
+    // Use this for initialization
+    void Start()
+    {
+        if (Advertisement.isSupported)
+        {
+            Advertisement.allowPrecache = true;
+            Advertisement.Initialize(appID, false);
+        }
+        googlePlayObj = GameObject.Find("GooglePlay");
+        googlePlayScript = googlePlayObj.GetComponent<Googleplay>();
+    }
 
-			if (fade)
-			{
-				Fade();
-			}
-		}
-		if (preCarrots != carrots) {
-			preCarrots = carrots;
-			carrotsCount.text = carrots.ToString();
-			if(googlePlayScript.ReturnSignInStatus() == true)
-			{
-				googlePlayScript.CarrotsAchievement(carrots);
-			}
-		}
-		if (preHunters != hunters) {
-			preHunters = hunters;
-			huntersCount.text = hunters.ToString();
-			if(googlePlayScript.ReturnSignInStatus() == true)
-			{
-				googlePlayScript.HunterAchievement(hunters);
-			}
-		}
-		if (preDay != sundial.day) {
-			preDay = sundial.day;
-			if(googlePlayScript.ReturnSignInStatus() == true)
-			{
-				googlePlayScript.DaysAchievement(sundial.day);
-			}
-		}
+    // Update is called once per physics time unit
+    void FixedUpdate()
+    {
+        //update day count
+        txtDayCount.text = "Day " + sundial.day.ToString();
 
-	}
+        if (player.activeSelf)
+        {
+            if (dayTime != sundial.isDayTime()) // if daytime has changed, update display msg
+            {
+                dayTime = sundial.isDayTime();
+                fade = true;
+                displayMsg = true;
 
-	public void GameOverDisplay(){
-		txtCarrots.text = "Carrots: " + carrots;
-		txtDays.text 	= "Days: " + sundial.day;
-		txtHunters.text = "Hunters: " + hunters;
-		new WaitForSeconds(1.0f);
-		txtGameOver.gameObject.SetActive(true);
-		txtCarrots.gameObject.SetActive(true);
-		txtDays.gameObject.SetActive(true);
-		txtHunters.gameObject.SetActive(true);
-		btnRestart.gameObject.SetActive(true);
-		btnShowLB.gameObject.SetActive(true);
-		btnExit.gameObject.SetActive (true);
-		btnPause.gameObject.SetActive (false);
-		if(lifeAvailable)
-		{
-			gameMusic.Pause();
-			btnShowAd.gameObject.SetActive(true);
-		}
-		googlePlayObj = GameObject.Find("GooglePlay");
-		googlePlayScript = googlePlayObj.GetComponent<Googleplay>();
-		if(googlePlayScript.ReturnSignInStatus() == false)
-		{
-			confirmLogin.SetActive(true);
-		} else
-		{
-			googlePlayScript.PostDaySurvived(sundial.day);
-			googlePlayScript.PostCarrotEaten(carrots);
-			googlePlayScript.PostHunterEaten(hunters);
-		}
-	}
+                if (dayTime)
+                {
+                    txtDayMsg.text = "Day " + sundial.day + "\n"
+                                    + "Run!";
+                }
+                else
+                {
+                    txtDayMsg.text = "Night " + sundial.day + "\n"
+                                    + "Hunt!";
+                }
+            }
 
-	public void HideGameOverDisplay(){
-		txtGameOver.gameObject.SetActive(false);
-		txtCarrots.gameObject.SetActive(false);
-		txtDays.gameObject.SetActive(false);
-		txtHunters.gameObject.SetActive(false);
-		btnRestart.gameObject.SetActive(false);
-		btnShowAd.enabled = false;
-		btnShowAd.gameObject.SetActive(false);
-		btnShowLB.gameObject.SetActive(false);
-		btnExit.gameObject.SetActive (false);
-		btnPause.gameObject.SetActive (true);
-	}
-	
-	public void Fade()
-	{
-		// if we try to display the msg, 
-		if(displayMsg)
-		{
-			txtDayMsg.CrossFadeAlpha(255, 2, false);
-			displayMsg = false;
-		} else
-		{
-			txtDayMsg.CrossFadeAlpha(0, 2, false);
-			fade = false;
-		}
-	}
+            if (fade)
+            {
+                Fade();
+            }
+        }
+        if (preCarrots != carrots)
+        {
+            preCarrots = carrots;
+            carrotsCount.text = carrots.ToString();
+            if (googlePlayScript.ReturnSignInStatus() == true)
+            {
+                googlePlayScript.CarrotsAchievement(carrots);
+            }
+        }
+        if (preHunters != hunters)
+        {
+            preHunters = hunters;
+            huntersCount.text = hunters.ToString();
+            if (googlePlayScript.ReturnSignInStatus() == true)
+            {
+                googlePlayScript.HunterAchievement(hunters);
+            }
+        }
+        if (preDay != sundial.day)
+        {
+            preDay = sundial.day;
+            if (googlePlayScript.ReturnSignInStatus() == true)
+            {
+                googlePlayScript.DaysAchievement(sundial.day);
+            }
+        }
 
-	public void Reviving(bool isReviving){
-		reviving = isReviving;
-	}
+    }
 
-	public bool IsReviving(){
-		return reviving;
-	}
+    public void GameOverDisplay()
+    {
+        txtCarrots.text = "Carrots: " + carrots;
+        txtDays.text = "Days: " + sundial.day;
+        txtHunters.text = "Hunters: " + hunters;
+        new WaitForSeconds(1.0f);
 
-	public void ShowRank(){
-		googlePlayObj = GameObject.Find("GooglePlay");
-		googlePlayScript = googlePlayObj.GetComponent<Googleplay>();
-		googlePlayScript.ShowLeaderboard();
-	}
+        gameOverDisplay.SetActive(true);
+        btnPause.gameObject.SetActive(false);
+        if (lifeAvailable)
+        {
+            gameMusic.Pause();
+            btnShowAd.gameObject.SetActive(true);
+        }
+        googlePlayObj = GameObject.Find("GooglePlay");
+        googlePlayScript = googlePlayObj.GetComponent<Googleplay>();
+        if (googlePlayScript.ReturnSignInStatus() == false)
+        {
+            confirmLogin.SetActive(true);
+        }
+        else
+        {
+            googlePlayScript.PostDaySurvived(sundial.day);
+            googlePlayScript.PostCarrotEaten(carrots);
+            googlePlayScript.PostHunterEaten(hunters);
+        }
+    }
 
-	public IEnumerator Wait(){
-		yield return new WaitForSeconds(5.0f);
-	}
+    public void HideGameOverDisplay()
+    {
+        gameOverDisplay.SetActive(false);
+        btnShowAd.enabled = false;
+        btnShowAd.gameObject.SetActive(false);
+        btnPause.gameObject.SetActive(true);
+    }
 
-	public void PostToLeaderboard(){
-		googlePlayObj = GameObject.Find("GooglePlay");
-		googlePlayScript = googlePlayObj.GetComponent<Googleplay>();
-		googlePlayScript.PostDaySurvived(sundial.day);
-		googlePlayScript.PostCarrotEaten(carrots);
-		googlePlayScript.PostHunterEaten(hunters);
+    public void Fade()
+    {
+        // if we try to display the msg, 
+        if (displayMsg)
+        {
+            txtDayMsg.CrossFadeAlpha(255, 2, false);
+            displayMsg = false;
+        }
+        else
+        {
+            txtDayMsg.CrossFadeAlpha(0, 2, false);
+            fade = false;
+        }
+    }
 
-	}
+    public void Reviving(bool isReviving)
+    {
+        reviving = isReviving;
+    }
+
+    public bool IsReviving()
+    {
+        return reviving;
+    }
+
+    public void ShowRank()
+    {
+        googlePlayObj = GameObject.Find("GooglePlay");
+        googlePlayScript = googlePlayObj.GetComponent<Googleplay>();
+        googlePlayScript.ShowLeaderboard();
+    }
+
+    public IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(5.0f);
+    }
+
+    public void PostToLeaderboard()
+    {
+        googlePlayObj = GameObject.Find("GooglePlay");
+        googlePlayScript = googlePlayObj.GetComponent<Googleplay>();
+        googlePlayScript.PostDaySurvived(sundial.day);
+        googlePlayScript.PostCarrotEaten(carrots);
+        googlePlayScript.PostHunterEaten(hunters);
+
+    }
 }
--- UnityProject/Assets/Scripts/ShowLeaderboard.cs
+++ UnityProject/Assets/Scripts/ShowLeaderboard.cs
@@ -2,54 +2,59 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class ShowLeaderboard : MonoBehaviour {
+public class ShowLeaderboard : MonoBehaviour
+{
+    public Button signOut;
 
+    private GameObject gp;
+    private Googleplay gpScript;
+    private Text signButtonText;
+    // Use this for initialization
+    void Start()
+    {
+        gp = GameObject.Find("GooglePlay");
+        gpScript = gp.GetComponent<Googleplay>();
+        bool status = gpScript.ReturnSignInStatus();
+        signButtonText = signOut.transform.FindChild("Text").GetComponent<Text>();
+        if (status == true)
+        {
+            signButtonText.text = "Signed Out";
+        }
+        else
+        {
+            signButtonText.text = "Signed In";
+        }
+    }
 
-	public Button signOut;
+    // Update is called once per frame
+    void Update()
+    {
 
-	private GameObject gp;
-	private Googleplay gpScript;
-	private Text signButtonText;
-	// Use this for initialization
-	void Start () {
-		gp = GameObject.Find("GooglePlay");
-		gpScript = gp.GetComponent<Googleplay>();
-		bool status = gpScript.ReturnSignInStatus();
-		signButtonText = signOut.transform.FindChild("Text").GetComponent<Text>();
-		if(status == true)
-		{
-			signButtonText.text = "Signed Out";
-		} else
-		{
-			signButtonText.text = "Signed In";
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	public void showLeaderboard(){
-		gp = GameObject.Find("GooglePlay");
-		gpScript = gp.GetComponent<Googleplay>();
-		gpScript.ShowLeaderboard();
+    }
+    public void showLeaderboard()
+    {
+        gp = GameObject.Find("GooglePlay");
+        gpScript = gp.GetComponent<Googleplay>();
+        gpScript.ShowLeaderboard();
 
-	}
+    }
 
-	public void signInOrOut(){
-		gp = GameObject.Find("GooglePlay");
-		gpScript = gp.GetComponent<Googleplay>();
-		bool status = gpScript.ReturnSignInStatus();
-		signButtonText = signOut.transform.FindChild("Text").GetComponent<Text>();
-		if(status == true)
-		{
-			gpScript.SignOut();
-			signButtonText.text = "Signed Out";
-		} else
-		{
-			gpScript.SignIn();
-			signButtonText.text = "Signed In";
-		}
-	}
+    public void signInOrOut()
+    {
+        gp = GameObject.Find("GooglePlay");
+        gpScript = gp.GetComponent<Googleplay>();
+        bool status = gpScript.ReturnSignInStatus();
+        signButtonText = signOut.transform.FindChild("Text").GetComponent<Text>();
+        if (status == true)
+        {
+            gpScript.SignOut();
+            signButtonText.text = "Signed Out";
+        }
+        else
+        {
+            gpScript.SignIn();
+            signButtonText.text = "Signed In";
+        }
+    }
 
 }
